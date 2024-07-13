ca590b8219584e9c12ed2e4513b630c0f6ba544a
wasp <wasp123321@gmail.com>
2024-07-13 16:48:23 +0200

    Finished backend setup

---
+++ FullstackResponsiveMERN/server/models/Post.js
@@ -0,0 +1,60 @@
+
+import mongoose from "mongoose"
+
+const postSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  location: String,
+  description: String,
+  picturePath: String,
+  userPicturePatch: String,
+  Likes: {
+    type: Map,
+    of: Boolean,
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+},
+{ timestamps: true }
+)
+
+postSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+export default Post
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
