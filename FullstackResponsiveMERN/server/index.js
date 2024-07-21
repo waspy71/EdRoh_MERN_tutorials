d543915526959346f363d4282bbb770580c6185b
wasp <wasp123321@gmail.com>
2024-07-21 17:28:32 +0200

    Friend List Widget

--- FullstackResponsiveMERN/server/index.js
+++ FullstackResponsiveMERN/server/index.js
@@ -62,8 +62,8 @@ mongoose.connect(process.env.MONGO_URL)
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
     /* ADD ONLY ONE TIME */
-    User.insertMany(users)
-    Post.insertMany(posts)
+    // User.insertMany(users)
+    // Post.insertMany(posts)
     })
   .catch((error) => console.log(`${error} did not connect`))
 
